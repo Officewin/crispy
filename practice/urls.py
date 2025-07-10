dd31ac4041ef2e531b4e1e503308150d9de7e76f
Officewin <104660484+Officewin@users.noreply.github.com>
2025-07-11 06:03:33 +0700

    Load topics from database

---
+++ practice/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'practice'
+
+urlpatterns = [
+    path('topics/', views.get_topics, name='topics'),
+    path('questions/', views.get_random_questions, name='questions'),
+    path('answer/<int:pk>/', views.submit_answer, name='answer'),
+]
