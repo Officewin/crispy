a9d3636a9e81295badc8b4f6b7b4dbc47b2488fb
Officewin <104660484+Officewin@users.noreply.github.com>
2025-07-10 21:49:37 +0700

    Fix template path for index

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'frontend/index.html'
+    # "frontend" is included in TEMPLATES['DIRS'], so we only need the
+    # template filename here. Using a simple name avoids the duplicated
+    # "frontend/frontend" path that caused TemplateDoesNotExist errors.
+    template_name = 'index.html'
